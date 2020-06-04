aec43f70f80e48e77862a16adcbe2e812e990879
Fred <fred@mslsr.com>
2020-06-04 11:17:46 +0200

    add error display to list View

--- app/src/view/list/index.js
+++ app/src/view/list/index.js
@@ -1,9 +1,22 @@
 import { h } from "preact";
+import { useContext } from "preact/hooks";
 
 import DreamList from "/src/component/DreamList";
 
+import { Store } from "/src/state/store";
+
 const View = () => {
-  return <DreamList />;
+  const { state } = useContext(Store);
+
+  return (
+    <div>
+      <DreamList />
+
+      {state.error && (
+        <div className="error w100 text-center">{state.error.message}</div>
+      )}
+    </div>
+  );
 };
 
 export default View;
