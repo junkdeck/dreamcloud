34d72a6941cc1310fc1635fbccc0d4fb19df213b
Fred <fred@mslsr.com>
2020-06-04 13:35:27 +0200

    setup webpack config
    generates an HTML file from app/index.html template
    cleans dist folder on every build
    compiles SASS/SCSS to CSS and exports to dist using
    MiniCssExtractPlugin. still requires an `import` in app/index.js
    sets up preact compatability, `src` import path
    built js is hashed and included in html build

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./app/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./app/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].[contenthash].js",
+  },
+  resolve: {
+    modules: ["node_modules"],
+    alias: {
+      react: "preact/compat",
+      "react-dom": "preact/compat",
+      src: path.resolve(__dirname, "app/src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /.s?[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3001,
+  },
+};
